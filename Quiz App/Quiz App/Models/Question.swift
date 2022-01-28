2490a00bad8c63863526e375daba201ed0e21eda
Kevin Watke <kjwatke@gmail.com>
2022-01-28 12:13:33 -0600

    initial setup, created models

---
+++ Quiz App/Quiz App/Models/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Quiz App
+//
+//  Created by Kevin  Watke on 1/28/22.
+//
+
+import Foundation
+
+struct Question {
+	
+	var question: String?
+	var answers: [String?]
+	var corretAnswerIndex: Int?
+	var feedback: String?
+}
---
+++ Quiz App/Quiz App/Models/QuizModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizModel.swift
+//  Quiz App
+//
+//  Created by Kevin  Watke on 1/28/22.
+//
+
+import Foundation
+
+class QuizModel {
+	
+	func getQuestions() {
+		
+	}
+}
